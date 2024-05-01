fbf4ffe29fa7f1a5a83b62a34b231338761783a1
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-02 02:30:13 +0530

    changes

--- to-do-list/src/Components/landing.jsx
+++ to-do-list/src/Components/landing.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React  from "react";
+import { useState,useEffect } from "react";
 import "./landing.css";
 import {Link} from "react-router-dom";
+
 function Landing(){
+    
     return(
         <div className="landing-wrapper">
             <div className="lan-subwrapper">
                 <p className="quote">
-                “Productivity is being able to do things that you were never able to do before.” <br/>-Franz Kafka
+                    
                 </p>
                 <Link to="/main"><p className="Button">Get Started</p></Link>
             </div>
