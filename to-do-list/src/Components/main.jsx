438b3a8f28f4ef3d1b30ffe4d4b2eb58dd1ec433
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-03 20:58:30 +0530

    changes

--- to-do-list/src/Components/main.jsx
+++ to-do-list/src/Components/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./main.css"
 import { useState } from "react";
 function Main(){
+  
     return(
         <div className="main-wrapper">
             <div className="main-subwrapper">
-                
+                <form>
+                    <input type="text" placeholder="Enter your task"></input>
+                    <button type="submit">Add</button>
+                </form>
             </div>
         </div>
     )
