df4e0c4f45732f8bfac006ec4ae5b366ad2022a4
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-08 23:39:30 +0530

    changes

--- nasa/src/Components/wrapper.jsx
+++ nasa/src/Components/wrapper.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import"./wrapper.css";
 import apod from "./apod.jpg"
 import Footer from "./Footer";
@@ -6,6 +6,25 @@ import { Info } from 'lucide-react';
 
 
 function Wrapper(){
+    const Key = process.env.REACT_APP_NASA_API_KEY;
+    console.log(process.env.REACT_APP_NASA_API_KEY);
+
+    useEffect(()=>{
+      async function fetchAPIData(){
+        const Key = process.env.REACT_APP_NASA_API_KEY;
+        console.log(Key);
+
+        // const url= 'https://api.nasa.gov/planetary/apod'+`?api_key=${Key}`
+        // try{
+        //     const res =await fetch(url)
+        //     const data = await res.json();
+        //     console.log("Data\n",data);
+        // }catch(err){
+        //     console.log(err.message)
+        // }
+      }
+      fetchAPIData ()
+    },[])
     return(
         <>
             
