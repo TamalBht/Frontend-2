08c06515bfac1247e2ba22a62076da7586379c09
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-14 22:02:09 +0530

    initial commit

--- brainwave/src/components/Button.jsx
+++ brainwave/src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ButtonSvg from "../assets/svg/ButtonSvg"
 const Button=({className, href, onClick,children , px,white})=>{
-    const classes = `button relative inline-flex items-center justify-center h11 transition-colors hover:text-color-1 ${px || "px-7"} ${white?"texxt-n-8":"text-n-1"} ${className||''}`;
-    const spanClasses=`relative z-10`;
+    const classes = `button    relative inline-flex items-center justify-center h11 transition-colors hover:text-color-1 ${px || "px-7"} ${white?"texxt-n-8":"text-n-1"} ${className||''}`;
+    const spanClasses=`relative z-10 item-center justify-self-center h-full mt-4`;
     const renderButton =()=>(
-        <button className={classes}>
+        <button className={classes} onClick={onClick}>
             <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
         </button>
