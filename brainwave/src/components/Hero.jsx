7f23f02be7b396e7890aea63d80663c662bbb66e
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-15 23:48:32 +0530

    changes

--- brainwave/src/components/Hero.jsx
+++ brainwave/src/components/Hero.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Section from './Section'
 import { curve, heroBackground, robot } from '../assets';
 import Button from './Button';
+import {BackgroundCircles , Gradient , BottomLine} from "./design/Hero"
+import {heroIcons} from '../constants'
+import { ScrollParallax } from 'react-just-parallax';
 
 const Hero = () => {
-  return (
+  const parallaxRef=useRef(null)
+    return (
     <Section className="pt-[12rem] mt-[8.25rem] overflow-hidden " crosses crossesOffset="lg:translate-y-[5.25rem]" customPadding id="#hero">
-        <div className='container relative overflow-hidden'>
-            <div className='relative z-1 max-w-[32rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]'>
-               <h1 className='h1 mb-6 block mt-5  lg:mb-6'>
+        <div className='container relative overflow-hidden' ref={parallaxRef}>
+            <div className='relative z-1 max-w-[32rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]'>
+               <h1 className='h1 mb-6 '>
                     Explore the Possibilities of AI Chatting with {``}
                     <span className='inline-block relative'>Brainwave{" "}<img src={curve} className='absolute top-full left-0 w-full xl:mt-2' width={624} height={28} alt='curve'/></span>
                 </h1>
@@ -27,13 +31,24 @@ const Hero = () => {
                         <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[668/490] lg:aspect-[1024/490]'>
                             <img src={robot} className='w-full scale-1.7 translate-y-[-8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]'  
                             alt='robot' width={1024} height={490}/>
+                            <ScrollParallax isAbsolutelyPositioned>
+                                <ul className='hidden absolute -left-[-5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex'>
+                                {heroIcons.map((icon,index)=>(
+                                    <li className='p-5' key={index}>
+                                        <img src={icon} width={24} height={25} alt='icon'/>
+                                    </li>
+                                ))}
+                                </ul>
+                            </ScrollParallax>
                         </div>
                     </div>
+                    <Gradient/>
 
                 </div>
                 <div className='absolute  -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[56%] md:w-[138%] lg:-top-[107%]' >
                     <img src={heroBackground} className='w-full ' width={1440} height={1800}/>
                 </div>
+                <BackgroundCircles/>
             </div>
         </div>
     </Section>
