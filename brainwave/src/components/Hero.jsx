85c50eaf2918be88ea8641f5ff5549eb50429b3a
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-16 00:28:35 +0530

    changes

--- brainwave/src/components/Hero.jsx
+++ brainwave/src/components/Hero.jsx
@@ -5,6 +5,7 @@ import Button from './Button';
 import {BackgroundCircles , Gradient , BottomLine} from "./design/Hero"
 import {heroIcons} from '../constants'
 import { ScrollParallax } from 'react-just-parallax';
+import Generating from './Generating';
 
 const Hero = () => {
   const parallaxRef=useRef(null)
@@ -31,6 +32,7 @@ const Hero = () => {
                         <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[668/490] lg:aspect-[1024/490]'>
                             <img src={robot} className='w-full scale-1.7 translate-y-[-8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]'  
                             alt='robot' width={1024} height={490}/>
+                            <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2"/>
                             <ScrollParallax isAbsolutelyPositioned>
                                 <ul className='hidden absolute -left-[-5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex'>
                                 {heroIcons.map((icon,index)=>(
