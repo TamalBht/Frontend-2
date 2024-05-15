8848b02f1c0ebcd096a23fcd55504eb379adaa9d
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-15 14:59:01 +0530

    hero update

--- brainwave/src/components/Hero.jsx
+++ brainwave/src/components/Hero.jsx
@@ -6,11 +6,11 @@ import Button from './Button';
 const Hero = () => {
   return (
     <Section className="pt-[12rem] mt-[8.25rem] overflow-hidden " crosses crossesOffset="lg:translate-y-[5.25rem]" customPadding id="#hero">
-        <div className='container relative'>
+        <div className='container relative overflow-hidden'>
             <div className='relative z-1 max-w-[32rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]'>
-               <h1 className='h1 mb-6 mt-5 lg:mb-6'>
-                Explore the Possibilities of AI Chatting with BrainwaveCurve
-                <span className='inline-block relative'>Brainwave{" "}<img src={curve} className='absolute top-full left-0 w-full xl:mt-2' width={624} height={28} alt='curve'/></span>
+               <h1 className='h1 mb-6 block mt-5  lg:mb-6'>
+                    Explore the Possibilities of AI Chatting with {``}
+                    <span className='inline-block relative'>Brainwave{" "}<img src={curve} className='absolute top-full left-0 w-full xl:mt-2' width={624} height={28} alt='curve'/></span>
                 </h1>
                 <p className='body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8'>
                     Unleash he power of AI wihin Brainwave. Upgrade your productivity with Brainwave, the open AI chat app
@@ -25,14 +25,14 @@ const Hero = () => {
                     <div className='relative bg-n-8 rounded-2xl'>
                         <div className='h-[1.4rem] bg-n-10 rounded-t-[0.9rem]'/>
                         <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[668/490] lg:aspect-[1024/490]'>
-                            <img src={robot} className='w-full'  
+                            <img src={robot} className='w-full scale-1.7 translate-y-[-8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]'  
                             alt='robot' width={1024} height={490}/>
                         </div>
                     </div>
 
                 </div>
-                <div className='absolute overflow-hidden -top-[54%] left-1/2 w-[234%] -translate-x-1/2' >
-                    <img src={heroBackground} className='w-full overflow-hidden' width={1440} height={1800}/>
+                <div className='absolute  -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[56%] md:w-[138%] lg:-top-[107%]' >
+                    <img src={heroBackground} className='w-full ' width={1440} height={1800}/>
                 </div>
             </div>
         </div>
