04282e70ac49daa49e4ed43f8a277a6a4f90e8f8
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-05-16 14:13:33 +0530

    changes

--- new/src/App.tsx
+++ new/src/App.tsx
@@ -1,3 +1,4 @@
+import { MatrixRainingLetters } from "react-mdr";
 
 import './App.css'
 function App() {
@@ -5,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        hello world
+        <MatrixRainingLetters/>
       </div>
      
     </>
